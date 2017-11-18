45c873f41135d2b19d608b5e025ef0b7263ee0f9
Elizabeth <elizabeth.ykw@gmail.com>
2017-11-18 15:13:08 +0800

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-hello
+# Telegram Reply Bot
+
+This is a telegram bot that will reply to messages when certain keywords are picked up in a group chat. It uses the Flask web framework and the Telegram Bot API to get chat updates via webhooks.
+
+Dependencies: telepot, flask
+___
+
+### How To Use
+
+After cloning the project from GitHub, you will need to create bot_details.py:
+
+```
+def get_botID(bot_name):
+    if bot_name == '<YOUR_BOT_NAME>':
+        return '<YOUR_BOT_KEY>'
+
+def get_serverURL():
+    return "<YOUR_SERVER_URL>"
+```
+
+`<YOUR_BOT_KEY>` is the one privided by BotFather and `<YOUR_SERVER_URL>` is the URL used to connect to the server. 
--- flask_app.py
+++ flask_app.py
@@ -3,12 +3,12 @@ from flask import Flask, request
 import urllib3
 from .bot_details import get_botID, get_serverURL
 
-### NEEDED FOR PYTHONANYWHERE DEPLOYMENT - comment out if not using pythonanywhere ###
-proxy_url = "http://proxy.server:3128"
-telepot.api._pools = {
-    'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
-}
-telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
+### NEEDED FOR PYTHONANYWHERE DEPLOYMENT - uncomment if using pythonanywhere ###
+# proxy_url = "http://proxy.server:3128"
+# telepot.api._pools = {
+#     'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
+# }
+# telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
 ### END ###
 
 bot = telepot.Bot(get_botID('althea'))
